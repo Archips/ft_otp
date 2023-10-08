113e5361d1b1f6d60151d79415fb9bf191924080
Archibald THIRION <athirion@paul-f3Br6s8.clusters.42paris.fr>
2023-10-08 17:03:23 +0200

    init repo

---
+++ ft_opt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-
+
+if __name__ == "__main__":
+
+    print("hello")
