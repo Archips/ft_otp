b69759d381c4e43b6cd059b2c8c588ec0e675fcc
Archibald THIRION <athirion@paul-f3Br6s8.clusters.42paris.fr>
2023-10-08 18:14:33 +0200

    parser in progress

--- ft_opt.py
+++ ft_opt.py
@@ -4,8 +4,20 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-
+    parser.add_argument("-g", help="hexadecimal key of 64 characters")
+    parser.add_argument("-k", help="generate a otp based on the key given in argument")
+    args = parser.parse_args()
+
+    if not args.g and not args.k:
+        parser.error("ft_opt takes at least one argument -g or -k")
+        exit(1)
+
+    return args
 
 if __name__ == "__main__":
 
+    args = parse_arguments()
+
     print("hello")
+    print(args.g)
+    print(args.k)
