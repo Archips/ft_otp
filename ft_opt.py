b669012f9cbbcd8415a72d60e5cf825079425503
Archibald THIRION <athirion@paul-f3Br5s2.clusters.42paris.fr>
2023-10-09 15:05:57 +0200

    totp generator done

--- ft_opt.py
+++ ft_opt.py
@@ -2,6 +2,10 @@
 
 import os
 import re
+import hmac
+import math
+import time
+import hashlib
 import argparse
 from cryptography.fernet import Fernet
 
@@ -86,6 +90,30 @@ def fernet_decrypt(encrypt_key):
     with open('decrypted_key.hex', 'wb') as decrypted_file:
         decrypted_file.write(decrypted)
 
+def ft_otp(encrypt_key, len: int = 6):
+
+    now = math.floor(time.time())
+    step = 30
+    t = math.floor(now / step)
+    shared_key = fernet_decrypt(encrypt_key)
+    hash = hmac.new(bytes(str(shared_key), 'utf-8'), t.to_bytes(length = 8, byteorder="big"), hashlib.sha256)
+    return dynamic_truncation(hash, len)
+
+def dynamic_truncation(hash: hmac.HMAC, len: int):
+    
+    bitstring = bin(int(hash.hexdigest(), base=16))
+    # >> 11010100000110011101010100010001100100011111001010111010001010110110000010111101000101011110111111010111101100011101001111100001011111101100001011110111100111001111100000100010011001010110101111100010111001001000010000011000000010010111100110101100011
+    last_four_bits = bitstring[-4:]
+    # >> 0011
+    offset = int(last_four_bits, base=2)
+    # >> 3
+    chosen_32_bits = bitstring[offset * 8 : offset * 8 + 32]
+    # >> 01000100011001000111110010101110
+    full_totp = str(int(chosen_32_bits, base=2))
+    # >> 1147436206
+    return full_totp[-len:]
+    # >> 436206
+
 if __name__ == "__main__":
 
     args = parse_arguments()
@@ -94,4 +122,4 @@ if __name__ == "__main__":
         encrypted_hex = check_hex(args.g)
         fernet_encrypt(encrypted_hex)
     if args.k and check_key(args.k):
-        fernet_decrypt(args.k)
+        print(ft_otp(args.k))
